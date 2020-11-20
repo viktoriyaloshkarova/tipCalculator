676d24360fc4121e0e6ff06a6b42c316a3b3b576
Viktoriya Loshkarova <viktoriya.loshkarova0419@gmail.com>
2020-11-20 16:22:35 -0500

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Viktoriya Loshkarova on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
